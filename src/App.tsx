049f3964215cde789e279b7a04a84b457436923b
FTP-droid <77394050+FTP-droid@users.noreply.github.com>
2022-11-11 20:08:51 -0500

    Configured user authenticated page. Added route for user authenticated page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LogIn from './components/LogIn';
+import UserPage from './components/UserPage'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LogIn isSignUpPage={false} />} />
-      <Route path="/signup" element={<LogIn isSignUpPage />} />
+      <Route path='/' element={<LogIn isSignUpPage={false} />} />
+      <Route path='/signup' element={<LogIn isSignUpPage />} />
+      <Route path='/user' element={<UserPage />} />
     </Routes>
   );
 }
---
+++ src/components/UserPage.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
+export default function SignUp() {
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
